186d3f05620655a11502ed475af859f3cdd93937
Fernando De Leon Gil <gasnare@hotmail.com>
2024-11-12 15:34:46 -0500

    Created Helper to get data gifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,4 +1,4 @@
-export const getGif = async (category) => {
+export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=y88zzx2vJ7wTjLeV4jLjoPy61O9fGfFH&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
@@ -6,9 +6,7 @@ export const getGif = async (category) => {
   const gifs = data.map((img) => ({
     id: img.id,
     title: img.title,
-    url: img.url,
+    url: img.images["downsized_medium"].url,
   }));
-
-  console.log("repos", gifs);
   return gifs;
 };
