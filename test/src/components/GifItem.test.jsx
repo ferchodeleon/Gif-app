8eaed019398b6d8ecd9564572e4cc4a3d4accabd
Fernando De Leon Gil <gasnare@hotmail.com>
2024-11-13 15:23:44 -0500

    Spelling fix

--- test/src/components/GifItem.test.jsx
+++ test/src/components/GifItem.test.jsx
@@ -6,22 +6,22 @@ describe("Test of component GifItem", () => {
   const url =
     "https://media1.giphy.com/media/5dY8E6UrHh5kF6nCBj/giphy-downsized-medium.gif?cid=bfd4d686b205aqvpy9f2ix2ukrl83qifv2vsffj0rk4sfvuh&ep=v1_gifs_search&rid=giphy-downsized-medium.gif&ct=g";
 
-  test("Most by math test with the snapshot of component GifItem", () => {
+  test("Must by math test with the snapshot of component GifItem", () => {
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
   });
 
-  test("Most by show the image and url indicate", () => {
+  test("Must by show the image and url indicate", () => {
     render(<GifItem title={title} url={url} />);
     expect(screen.getByRole("img").src).toBe(url);
   });
 
-  test("Most by show the alt indicate to by image", () => {
+  test("Must by show the alt indicate to by image", () => {
     render(<GifItem title={title} url={url} />);
     expect(screen.getByRole("img").alt).toBe(title);
   });
 
-  test("Most by show title of card component", () => {
+  test("Must by show title of card component", () => {
     render(<GifItem title={title} url={url} />);
     expect(screen.getByTestId("testTitleCard").innerHTML).toBe(title);
   });
